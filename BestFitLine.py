cf9c1d82614bff3d33daecbb90680b7f1cf9943c
EragonBromson <sarthak.agrawal76@gmail.com>
2017-08-18 15:44:25 +0530

    Coefficient of Determination determined

--- BestFitLine.py
+++ BestFitLine.py
@@ -16,6 +16,17 @@ def yIntercept(xs, ys, m):
     b = mean(ys) - m*mean(xs)
     return b
 
+
+def squaredError(ysOriginal, ysLine):
+    return sum((ysOriginal-ysLine)*(ysOriginal-ysLine))
+
+
+def coeffOfDetermination(ysOriginal, ysLine):
+    yMean = [mean(ysOriginal) for y in ysOriginal]
+    squaredErrorRegression = squaredError(ysOriginal, ysLine)
+    squaredErrorMean = squaredError(ysOriginal, yMean)
+    return 1 - (squaredErrorRegression / squaredErrorMean)
+
 xs = np.array([1, 2, 3, 4, 5], dtype=np.float64)
 ys = np.array([5, 4, 6, 5, 6], dtype=np.float64)
 
@@ -26,11 +37,15 @@ print(b)
 
 regressionLine = [m*x + b for x in xs]
 
-predictX = 7
-predictY = m*predictX + b
-print(predictY)
-plot.scatter(xs, ys, color="Blue", label="Data")
-plot.plot(xs, regressionLine, label="Best-fit line")
-plot.scatter(predictX, predictY, label="Prediction", color="Green")
-plot.legend(loc=4)
-plot.show()
+rSquare = coeffOfDetermination(ys, regressionLine)
+
+print(rSquare)
+
+# predictX = 7
+# predictY = m*predictX + b
+# print(predictY)
+# plot.scatter(xs, ys, color="Blue", label="Data")
+# plot.plot(xs, regressionLine, label="Best-fit line")
+# plot.scatter(predictX, predictY, label="Prediction", color="Green")
+# plot.legend(loc=4)
+# plot.show()
