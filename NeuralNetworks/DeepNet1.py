205bd7e4b4c4972c6db333d42ad754143a61e65b
EragonBromson <sarthak.agrawal76@gmail.com>
2017-10-31 12:53:29 +0530

    Done: Deep Learning with TensorFlow - Creating the Neural Network Model p.46

---
+++ NeuralNetworks/DeepNet1.py
@@ -0,0 +1,41 @@
+import tensorflow as tf
+
+from tensorflow.examples.tutorials.mnist import input_data
+
+#  one_hot parameter there. The term comes from electronics where just one element, out of the others, is literally "hot," or on.
+mnist = input_data.read_data_sets("/tmp/data/", one_hot=True)
+
+
+nNodesHl11 = 500
+nNodesHl12 = 500
+nNodesHl13 = 500
+
+nClasses = 10
+batchSize = 100
+
+x = tf.placeholder('float', [None, 784])
+y = tf.placeholder('float')
+
+
+def neuralNetworkModel(data):
+    hiddenLayer1 = {'weights':tf.Variable(tf.random_normal([784, nNodesHl11])),
+                    'biases':tf.Variable(tf.random_normal([nNodesHl11]))}
+    hiddenLayer2 = {'weights': tf.Variable(tf.random_normal([nNodesHl11, nNodesHl12])),
+                    'biases': tf.Variable(tf.random_normal([nNodesHl12]))}
+    hiddenLayer3 = {'weights': tf.Variable(tf.random_normal([nNodesHl12, nNodesHl13])),
+                    'biases': tf.Variable(tf.random_normal([nNodesHl13]))}
+    outputLayer = {'weights': tf.Variable(tf.random_normal([nNodesHl13, nClasses])),
+                    'biases': tf.Variable(tf.random_normal([nClasses]))}
+    l1 = tf.add(tf.matmul(data, hiddenLayer1['weights']), hiddenLayer1['biases'])
+    l1 = tf.nn.relu(l1)
+
+    l2 = tf.add(tf.matmul(l1, hiddenLayer2['weights']), hiddenLayer2['biases'])
+    l2 = tf.nn.relu(l2)
+
+    l3 = tf.add(tf.matmul(l2, hiddenLayer3['weights']), hiddenLayer3['biases'])
+    l3 = tf.nn.relu(l3)
+
+    output = tf.add(tf.matmul(l3, outputLayer['weights']), outputLayer['biases'])
+    return output
+
+
