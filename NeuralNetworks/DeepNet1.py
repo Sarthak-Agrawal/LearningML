54676871aa7444cc99ab686672a5759d8080d335
EragonBromson <sarthak.agrawal76@gmail.com>
2018-01-28 05:00:39 +0530

    Done: Running our Network - Deep Learning with Neural Networks and TensorFlow p.46

--- NeuralNetworks/DeepNet1.py
+++ NeuralNetworks/DeepNet1.py
@@ -1,10 +1,11 @@
+import os
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+
 import tensorflow as tf
 
 from tensorflow.examples.tutorials.mnist import input_data
 
-#  one_hot parameter there. The term comes from electronics where just one element, out of the others, is literally "hot," or on.
-mnist = input_data.read_data_sets("/tmp/data/", one_hot=True)
-
+mnist = input_data.read_data_sets("data/", one_hot=True)
 
 nNodesHl11 = 500
 nNodesHl12 = 500
@@ -15,7 +16,6 @@ batchSize = 100
 
 x = tf.placeholder('float', [None, 784])
 y = tf.placeholder('float')
-
 
 def neuralNetworkModel(data):
     hiddenLayer1 = {'weights':tf.Variable(tf.random_normal([784, nNodesHl11])),
@@ -44,6 +44,20 @@ def trainNeuralNetwork(x):
     cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=prediction, labels=y))
     optimizer = tf.train.AdamOptimizer().minimize(cost)
     hmEpochs = 10
-    with tf.Session as session:
-        session.run(tf.global_variables_initializer())
+    with tf.Session() as sess:
+        sess.run(tf.global_variables_initializer())
+        for epoch in range(hmEpochs):
+            epochLoss = 0
+            for _ in range(int(mnist.train.num_examples/batchSize)):
+                XBatch, YBatch = mnist.train.next_batch(batchSize)
+                _, l = sess.run([optimizer, cost], feed_dict={x: XBatch, y:YBatch})
+                epochLoss += l
 
+            print('Epoch', epoch, 'completed out of', hmEpochs, 'loss:', epochLoss/int(mnist.train.num_examples/batchSize))
+
+        correctPreds = tf.equal(tf.argmax(prediction, 1), tf.argmax(y, 1), name="CorrectPreds")
+        accuracy = tf.reduce_mean(tf.cast(correctPreds, 'float'))
+        print("Accuracy : ", accuracy.eval({x:mnist.test.images, y:mnist.test.labels}))
+
+
+trainNeuralNetwork(x)
